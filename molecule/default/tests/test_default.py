de9b6454c33fbf6ec9c4e54c41d9d55723bd56b0
Sebastien Besson <seb.besson@gmail.com>
2020-04-14 16:55:53 +0100

    Update Molecule tests to check file content

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,13 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_sudoers(host):
+    f1 = host.file('/etc/sudoers')
+    assert f1.exists
+    assert f1.contains('#includedir /etc/sudoers.d')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+def test_sudoers_individual_commands(host):
+    f2 = host.file('/etc/sudoers.d/sudoers-individual_commands')
+    assert f2.exists
+    assert f2.contains('%root ALL=(ALL) NOPASSWD: ALL')
